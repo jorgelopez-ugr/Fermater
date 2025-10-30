d2056c83eeffcf86c1b66aabfa193b30784d26de
Gabriel Herrera <gabrielherrera@correo.ugr.es>
2025-10-30 13:14:47 +0100

    Eliminado código sin issue asignado

--- src/domain/entities/Venta.js
+++
@@ -1,9 +0,0 @@
-class Venta{
-    constructor(importe_venta, fecha, id_cosecha){
-        this.importe_venta = importe_venta;
-        this.fecha = fecha;
-        this.id_cosecha = id_cosecha;
-    }
-}
-
-module.exports = Venta;
